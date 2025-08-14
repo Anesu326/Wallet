d62f616cce939aa949758a77d409722a9b35597c
Anesu Ndamuka <anesundamuka@gmail.com>
2025-08-14 16:02:39 +0200

    User account view

---
+++ wallet/accounts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+
+class RegisterForm(forms.ModelForm):
+    name = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label='Email')
+
+class SendMoneyForm(forms.Form):
+    amount = forms.DecimalField(min_value=10, max_value=10000)
+    currency = forms.ChoiceField(choices=[('GBP', 'GBP'), ('ZAR', 'ZAR')])
+    
--- wallet/accounts/views.py
+++ wallet/accounts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, LoginForm, SendMoneyForm
+from .models import Profile
+from django.contrib.auth.models import User
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(username=form.cleaned_data['email'],
+                                            email = form.cleaned_data['email'], password=form.cleaned_data['password'])
+            user.profile.name = form.cleaned_data['name']
+            user.profile.save()
+            return redirect('login')
+        else:
+            form = RegisterForm()
+            return render(request, 'accounts/register.html', {'form':form})
+        
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('dashboard')
+        else:
+            form = LoginForm()
+            return render(request, 'accounts/login.html', {'form':form})
+
+@login_required
+def dashboard_view(request):
+    result = None
+    if request.method == 'POST':
+        form = SendMoneyForm(request.POST)
+        if form.is_valid():
+            amount = form.cleaned_data['amount']
+            currency = form.cleaned_data['currency']
+            fx_rates = {'GBP':0.78, 'ZAR':18.2}
+            fees = 
 
 # Create your views here.
