771b26a1bea5dd2ef5c57d6084fea15468c0e56e
Anesu Ndamuka <anesundamuka@gmail.com>
2025-08-14 18:13:54 +0200

    Transaction receipt feature

--- wallet/accounts/models.py
+++ wallet/accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 
 @receiver(post_save, sender=User)
 
@@ -13,3 +14,15 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount_usd = models.DecimalField(max_digits=12, decimal_places=2)
+    currency = models.CharField(max_length=3)
+    fee = models.DecimalField(max_digits=12, decimal_places=2)
+    final_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    fx_rate = models.DecimalField(max_digits=12, decimal_places=4)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def _str_(self):
+        return f"{self.user.email} sent {self.final_amount}{self.currency} on {self.timestamp.strftime('%Y-%m-%d %H:%M')}"
+    
