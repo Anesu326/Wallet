b0a91e40f7a376f6d1764d581d0eb3d2eabb2631
Anesu Ndamuka <anesundamuka@gmail.com>
2025-08-15 09:54:39 +0200

    Adding name to the welcome section

--- wallet/accounts/views.py
+++ wallet/accounts/views.py
@@ -10,7 +10,7 @@ def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(username=form.cleaned_data['email'],
+            user = User.objects.create_user(username=form.cleaned_data['name'],
                                             email = form.cleaned_data['email'], 
                                             password=form.cleaned_data['password'])
             user.profile.name = form.cleaned_data['name']
